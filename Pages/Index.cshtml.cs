fed9c8d9cf60e6385bf895a286d9c0bf40c8c667
DVDeusVultDV <crazygamergmr@gmail.com>
2023-03-26 22:22:48 +0300

    Добавьте файлы проекта.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nedflix.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public class Movie
+        {
+            //string name { get; set; }
+            //string director { get; set; }
+            //string genre { get; set; }
+            //string description { get; set; }
+            //string сеансы
+            Movie() { }
+        }
+        //public string msg { get; set; }
+        public IndexModel()
+        {
+
+        }
+
+        public async void OnGet()
+        {
+            
+            //https://kinopoiskapiunofficial.tech/api/films/get_api_v2_2_films_filters
+        }
+    }
+}
