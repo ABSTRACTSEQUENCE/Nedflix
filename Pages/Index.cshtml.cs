a03bdfc9768ff95deae5b1b905be9bc5fd272552
DVDeusVultDV <crazygamergmr@gmail.com>
2023-04-02 19:48:58 +0300

    HW DONE, но осталось только вывод нормальный сделать

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,14 +11,14 @@ namespace Nedflix.Pages
 		{
 			public class Movie
 			{
+				public int kinopoiskId { get; set; }
 				public class Genres
 				{
 					public string genre { get; set;}
 				}
 				public List<Genres> genres { get; set; }
 				public string nameRu { get; set; }
-				//public string genre { get; set; }
-				public string description { get; set; }
+				//public string description { get; set; }
 				//string сеансы
 
 			}
@@ -26,26 +26,16 @@ namespace Nedflix.Pages
 			public List<Movie> items { get; set; }
 			public MovieList() { }
 		}
-		//public string msg { get; set; }
-		public IndexModel()
-		{
-
-		}
-
-		public async void OnPost()
-		{
-			//Movie mov = await Request.ReadFromJsonAsync<Movie>();
-		}
-
+		public IndexModel() {}
+		public MovieList mList;
 		public async void OnGet()
 		{
 			string url = "https://kinopoiskapiunofficial.tech/api/v2.2/films?/items";
 			var client = new HttpClient();
 			client.DefaultRequestHeaders.Add("Accept", "application/json");
 			client.DefaultRequestHeaders.Add("X-API-KEY", "c278dfaf-f118-4523-8b32-831ddded96b1");
-			//var result = await client.GetStringAsync(url);
-			var result = await client.GetAsync(url).Result.Content.ReadFromJsonAsync<MovieList>();
-			//Movie mov = await result.Content.ReadFromJsonAsync<Movie>();
+			mList = await client.GetAsync(url).Result.Content.ReadFromJsonAsync<MovieList>();
+			//await HttpContext.Response.WriteAsJsonAsync(result);
 		}
 	}
 }
